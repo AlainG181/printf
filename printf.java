a5aafc62093dae02e1afeccd0fbb1b706e09691c
Mindusting <128253784+Mindusting@users.noreply.github.com>
2023-03-18 19:53:45 +0100

    Add files via upload

---
+++ printf.java
@@ -0,0 +1,12 @@
+public class printf{
+    public static void main(String[] args) {
+        //Variables
+        String name = "Mindusting";
+        byte age = 18;
+        float height = 1.65f;
+
+        System.out.printf(
+            "Hola, mi nombre es %s tengo %d años y mido %.1f.\n",
+            name, age, height);
+    }
+}
